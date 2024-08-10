c43ccb2f3e2e4c801ca949f1e59c71da9a9b7280
rupambsws9 <rupambsws9@gmail.com>
2024-08-11 00:43:53 +0530

    add 16.cpp

---
+++ Operators/16.cpp
@@ -0,0 +1,13 @@
+//constants in c++
+#include<iostream>
+using namespace std;
+
+int main() {
+    const float PI = 3.14;
+    const float g = 9.8;
+
+    cout << "The value of PI is " << PI << endl;
+    cout << "The value of gravity (g) is " << g << " m/s^2" << endl;
+
+    return 0;
+}
