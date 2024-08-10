7990682cc805d47407153cbf678f7a9682653a57
rupambsws9 <rupambsws9@gmail.com>
2024-08-10 22:46:26 +0530

    add 12.cpp

---
+++ VariableAndDataType/12.cpp
@@ -0,0 +1,11 @@
+//In a program,input the side of asquare.You have to output the area of the square.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int side;
+    cout <<"Enter the Side of the square: ";
+    cin>>side;
+    cout<<"The area of the square is: "<<side*side<<endl;
+    return 0;
+}
