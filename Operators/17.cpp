fc3787800da6b9d04d86e55ca757a3d9a63b97f5
rupambsws9 <rupambsws9@gmail.com>
2024-08-11 00:59:47 +0530

    add 17.cpp

---
+++ Operators/17.cpp
@@ -0,0 +1,10 @@
+//implicit type conversions in c++
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << (10/3) << endl; // 3
+    cout <<(10/3.0) << endl; // 3.33333
+    cout <<('A'+1) << endl; // 66
+    return 0;
+}
