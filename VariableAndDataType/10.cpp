02cd73177e3f766cf91ef4db47f32f4e1f729a98
rupambsws9 <rupambsws9@gmail.com>
2024-08-10 22:25:54 +0530

    add 10.cpp

--- VariableAndDataType/10.cpp
+++ VariableAndDataType/10.cpp
@@ -1,11 +1,14 @@
-//Addition of two numbers
+//addition of two numbers by using user input
 #include<iostream>
 using namespace std;
 
 int main(){
-    int a=1;
-    int b=2;
-    int sum = a + b;
-    cout<<"Sum: "<<sum<<endl;
+    int a;
+    int b;
+    cout <<"Enter First number: ";
+    cin >> a;
+    cout <<"Enter Second number: ";
+    cin >> b;
+    cout << "Addition of " << a << " and " << b << " is: " << a+b << endl;
     return 0;
 }
---
+++ VariableAndDataType/8.cpp
@@ -0,0 +1,11 @@
+//input in c++
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Your age is " << age << endl;
+    return 0;
+}
