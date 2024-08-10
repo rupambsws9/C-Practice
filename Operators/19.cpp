c60763849ef51c8564ce32788aded6c6298e12b5
rupambsws9 <rupambsws9@gmail.com>
2024-08-11 01:19:17 +0530

    add 19.cpp

---
+++ Operators/19.cpp
@@ -0,0 +1,9 @@
+//predict output of (bool)3+5; and (bool)0+5;
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << (bool)3+5 << endl; // 6
+    cout << (bool)0+5 << endl; //5
+    return 0;
+}
