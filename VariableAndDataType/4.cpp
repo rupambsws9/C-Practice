6182ae4ecedfe336dbde6a0c750c61072304fdea
rupambsws9 <rupambsws9@gmail.com>
2024-08-10 21:16:02 +0530

    add 4.cpp and 5.cpp

---
+++ VariableAndDataType/4.cpp
@@ -0,0 +1,11 @@
+//Variables in c++
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=1;
+    int b=2;
+    cout<< a <<endl;
+    cout<< b <<endl;
+    return 0;
+}
---
+++ VariableAndDataType/5.cpp
@@ -0,0 +1,11 @@
+//Addition of two numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=1;
+    int b=2;
+    int sum = a + b;
+    cout<<"Sum: "<<sum<<endl;
+    return 0;
+}
