a531a200a1f47f34bcc904eae9fa78c4ce4f4d9d
rupambsws9 <rupambsws9@gmail.com>
2024-08-12 00:22:54 +0530

    add 38.cpp

---
+++ Loops/38.cpp
@@ -0,0 +1,14 @@
+//for loop in c++
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number of range: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
