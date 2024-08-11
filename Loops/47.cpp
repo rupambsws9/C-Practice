afeb548a11fdabd487a4fac136b0afb738fc0621
rupambsws9 <rupambsws9@gmail.com>
2024-08-12 02:15:55 +0530

    add 47.cpp

---
+++ Loops/47.cpp
@@ -0,0 +1,14 @@
+//do while loop in c++
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    do {
+        cout << i << " ";
+        i++;
+    } while (i <= 5);
+    cout << endl;
+
+    return 0;
+}
