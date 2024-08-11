0596007c51a2f2bba7117c6fe76a61625a3aecf9
rupambsws9 <rupambsws9@gmail.com>
2024-08-12 03:41:40 +0530

    add 54.cpp

---
+++ Loops/54.cpp
@@ -0,0 +1,27 @@
+//check if a number is prime or not in c++(optimised)
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int num;
+    bool isPrime = true;
+    cout << "Enter a number: ";
+    cin>>num;
+    for(int i=2;i<=sqrt(num);i++){
+        if(num%i==0){
+            isPrime = false;
+            break;
+        }
+    }
+    if(num==1){
+        cout <<"The number " <<num<<" is not prime." << endl;
+    }else{
+    if(isPrime==true){
+        cout <<"The number " <<num<<" is prime." << endl;
+    }else{
+        cout <<"The number " <<num<<" is not prime." << endl;
+    }
+    }
+return 0;
+}
