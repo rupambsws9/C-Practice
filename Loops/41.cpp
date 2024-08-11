9569a1d9dc8975412b7b4f17aee5fcbb5df3ae9e
rupambsws9 <rupambsws9@gmail.com>
2024-08-12 00:59:00 +0530

    add 41.cpp

---
+++ Loops/41.cpp
@@ -0,0 +1,14 @@
+//print 10 to 100 by using while loop in c++
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int num = 10;
+
+    while (num <= 100) {
+        cout << num << " ";
+        num++;
+    }
+    cout << endl;
+    return 0;
+}
