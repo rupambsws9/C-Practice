837c563d47d114176feec0c0419ca6c92093eca8
rupambsws9 <rupambsws9@gmail.com>
2024-08-12 01:08:27 +0530

    add 42.cpp

---
+++ Loops/42.cpp
@@ -0,0 +1,16 @@
+/*print squre star pattern like -
+    ****
+    ****
+    ****
+    ****
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    for(int i =0;i<4;i++){
+        cout << "****" << " "<< endl;
+    }
+    return 0;
+}
