a9b8ff38705b9523c7c801be094a2522b3b30b19
rupambsws9 <rupambsws9@gmail.com>
2024-08-18 22:50:41 +0530

    add 55.cpp

--- Loops/55.cpp
+++ Loops/55.cpp
@@ -1,1 +1,14 @@
-//factorial
+//WAP to find the Factorial of a number entered by the user in c++
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num, fact;
+    cout << "Enter the number that you want to find factorial: ";
+    cin >> num;
+    for(int i=num; i>0; i--){
+        fact *= i;
+    }
+    cout << "The factorial of " << num << " is: " << fact << endl;
+    return 0;
+}
